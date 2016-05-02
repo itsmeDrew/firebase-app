012de9469d3b30405541a18a0028c4f4a6582cc6
Drew Roberts <drew@itsmedrew.com>
2016-05-02 15:49:48 -0400

    base set styling / slick slider implemented

--- app/src/js/controllers/CategoryCtrl.js
+++ app/src/js/controllers/CategoryCtrl.js
@@ -8,6 +8,38 @@ function CategoryCtrl ($stateParams, $state, $scope) {
   var vm = this;
   vm.setSlug = $stateParams.setSlug;
   vm.setId = $stateParams.setId;
+
+  vm.slickConfig = {
+    centerMode: true,
+    infinite: true,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 970,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  }
 };
 
 module.exports = CategoryCtrl;
