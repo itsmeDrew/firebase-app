800c7143f3377d8ba246f46c31225a8c7be95581
Drew Roberts <drew@itsmedrew.com>
2016-05-03 17:50:11 -0400

    assign admin user role on initial sign in

--- app/src/js/services/users.js
+++ app/src/js/services/users.js
@@ -6,7 +6,8 @@ app.service('users', UsersCtrl);
 
 function UsersCtrl ($firebaseAuth, config) {
   var vm = this;
-  var baseRef = new Firebase(config.baseDataURL);
+  var baseDataURL = config.baseDataURL;
+  var baseRef = new Firebase(baseDataURL);
   var baseAuth = $firebaseAuth(baseRef);
 
   vm.authHandler = authHandler;
@@ -33,17 +34,19 @@ function UsersCtrl ($firebaseAuth, config) {
       if (authData) {
         var _userList = ref.child("users");
 
+        console.log(isAdmin(authData.uid), authData.uid);
+
         vm.authenticated = false;
 
         _userList.once('value', function(snapshot) {
           var _userId = authData.uid;
 
           if (authData && !snapshot.hasChild(_userId) && !vm.authenticated) {
-            console.log('new user');
+            // NEW USER
             setNewUser(_userList, _userId, authData);
             vm.authenticated = true;
           } else if (authData && snapshot.hasChild(_userId) && !vm.authenticated) {
-            console.log('returning user');
+            // RETURNING USER
             vm.authenticated = true;
           }
         });
@@ -51,12 +54,36 @@ function UsersCtrl ($firebaseAuth, config) {
     });
   }
 
+  function isAdmin(id) {
+    var _adminList = baseRef.child("admins");
+
+    _adminList.once('value', function(snapshot) {
+      return true;
+    })
+  }
+
   function setNewUser(list, userId, authData) {
-    list.child(userId).set({
-      provider: authData.provider,
-      name: getName(authData),
-      userID: userId
-    });
+    var _adminList = baseRef.child("admins");
+
+    _adminList.once('value', function(snapshot) {
+      if (snapshot.hasChild(authData.uid)) {
+        list.child(userId).set({
+          provider: authData.provider,
+          name: getName(authData),
+          userID: userId,
+          role: 20
+        });
+      } else {
+        list.child(userId).set({
+          provider: authData.provider,
+          name: getName(authData),
+          userID: userId,
+          role: 10
+        });
+      }
+    })
+
+
   }
 
   function getName(authData) {
